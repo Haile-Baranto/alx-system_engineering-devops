8d5901eaf2d74e5571aea9fa50950de94a2c1aaf
Haile-Baranto <hailebaranto@gmail.com>
2023-06-30 22:49:38 +0300

    Edit soln to soln to 102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,49 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
 
-/*
- * This program demonstrates the creation of zombie processes.
- * It creates 5 child processes that immediately exit, becoming zombies.
- * The parent process prints the PID of each zombie process created.
+/**
+ * infinite_while - Run an infinite while loop.
+ *
+ * This function runs an infinite while loop using the sleep function.
+ * It is used to keep the program running after creating the zombie processes.
+ *
+ * Return: Always 0.
  */
-
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+		sleep(1);
+
+	return (0);
 }
 
+/**
+ * main - Creates five zombie processes.
+ *
+ * This function creates five child processes using fork.
+ * The parent process prints the PID of the zombie processes and continues.
+ * The child processes exit immediately, becoming zombies.
+ * The program then enters an infinite while loop to keep running.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    int i;
-    pid_t child_pid;
+	pid_t pid;
+	int count;
 
-    // Create 5 child processes that immediately exit
-    for (i = 0; i < 5; i++)
-    {
-        child_pid = fork();
+	// Create five zombie processes
+	for (count = 0; count < 5; count++)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			// Parent process
+			printf("Zombie process created, PID: %d\n", pid);
+		}
+		else if (pid == 0)
+		{
+			// Child process
+			exit(0);
+		}
+		else
+		{
+			// Fork failed
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+	}
 
-        if (child_pid > 0) // Parent process
-        {
-            printf("Zombie process created, PID: %d\n", child_pid);
-            sleep(1);
-        }
-        else if (child_pid == 0) // Child process
-        {
-            exit(0);
-        }
-        else // Forking error
-        {
-            perror("Forking error");
-            exit(1);
-        }
-    }
+	// Keep the program running
+	infinite_while();
 
-    infinite_while();
-
-    return (0);
+	return (0);
 }
