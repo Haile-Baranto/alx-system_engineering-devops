0a338e5fa48d5de9228a8f27df6faf60ae2717cf
Haile-Baranto <hailebaranto@gmail.com>
2023-10-11 23:04:37 +0300

    Fix 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,26 +1,12 @@
-# 0-strace_is_your_friend.pp
+# Ensure WordPress file wp-settings.php has correct PHP extensions
 
-# Define a file resource to manage the Apache configuration file
-file { '/etc/apache2/sites-available/my-site.conf':
-  ensure  => present,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  content => template('apache/my-site.conf.erb'),
-  require => Package['apache2'],  # Ensure Apache is installed before managing the config
+file { '/var/www/html/wp-settings.php':
+  ensure => file,
 }
 
-# Define an Apache vhost resource to enable the site
-apache::vhost { 'my-site':
-  vhost_name    => 'my-site',
-  docroot       => '/var/www/html',
-  vhost_aliases => '*',
-  require       => File['/etc/apache2/sites-available/my-site.conf'],
-  notify        => Service['apache2'],
-}
-
-# Ensure Apache service is running
-service { 'apache2':
-  ensure => 'running',
-  enable => true,
+exec { 'fix-wordpress-php-extensions':
+  command     => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path        => ['/usr/local/bin', '/bin'],
+  refreshonly => true,
+  subscribe   => File['/var/www/html/wp-settings.php'],
 }
