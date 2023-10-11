fa6f546e8dd399f7d3efd3b2b96f69dbad37b661
Haile-Baranto <hailebaranto@gmail.com>
2023-10-11 23:11:03 +0300

    Fix 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,7 @@
+#!/usr/bin/env puppet
 # Ensure WordPress file wp-settings.php has correct PHP extensions
-
-file { '/var/www/html/wp-settings.php':
-  ensure => file,
-}
-
-exec { 'fix-wordpress-php-extensions':
-  command     => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path        => ['/usr/local/bin', '/bin'],
-  refreshonly => true,
-  subscribe   => File['/var/www/html/wp-settings.php'],
+exec { 'web stack deubging':
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path     => '/usr/local/bin/:/bin/',
+  provider => 'shell'
 }
